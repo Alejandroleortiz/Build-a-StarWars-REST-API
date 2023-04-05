a8a1c6217fa71e9c2e7920d35851b76d45ad4bf7
Alejandroleortiz <alejandroleortiz57@gmail.com>
2023-04-05 22:59:00 +0000

    terminado modelo

--- src/models.py
+++ src/models.py
@@ -11,8 +11,8 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), default=True)
     suscription_date = db.Column(db.String, nullable=False)
-    character_id = db.relationship(foreign_keys="[character.user_id]", backref="character_id")
-    planet_id = db.relationship(foreign_keys="[planet.user_id]", backref="planet_id")
+    characters = db.relationship('Character', secondary="favorite_character", backref="user")
+    planets = db.relationship('Planet', secondary="favorite_planet", backref="user")
 
 class Character(db.Model):
     __tablename__ = 'character'
@@ -31,18 +31,15 @@ class Planet(db.Model):
 class Favorite_character(db.Model):
     __tablename__ = 'favorite_character'
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     character_id = db.Column(db.Integer, db.ForeignKey('character.uid'), primary_key=True)
-    user = db.relationship('User')
-    character = db.relationship('Character', backref="character")
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    
 
 class Favorite_planet(db.Model):
     __tablename__ = 'favorite_planet'
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     planet_id = db.Column(db.Integer, db.ForeignKey('planet.uid'), primary_key=True)
-    user = db.relationship('User')
-    planet = db.relationship('Planet', backref="planet")
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     
     # def __repr__(self):
     #     return '<User %r>' % self.username
